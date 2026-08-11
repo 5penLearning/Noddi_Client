ad6c77560a5a719a48c26358193e2a5311ed887e
uptomaster <lnh929@naver.com>
2026-08-12 03:04:43 +0900

    [Feat] : 회의 페이지 실시간 날짜 및 캘린더 기능 구현

---
+++ src/hooks/useCurrentDateTime.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+function useCurrentDateTime() {
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return now;
+}
+
+export default useCurrentDateTime;
